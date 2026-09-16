f47c7bbab70d53ec2edd2bff68fa27928067a05b
yj438863687 <yj438863687@users.noreply.github.com>
2026-09-16 15:58:21 +0800

    新增拍照识图搜同款功能（Google Lens/必应/Yandex/百度识图）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // OmniSearch Service Worker - 缓存壳资源，实现离线可打开
-const CACHE = 'omnisearch-v1';
+const CACHE = 'omnisearch-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
